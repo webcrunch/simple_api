43db6c8591f279a7f71846ef8ebd1fa55367fbb1
Jarl Holmberg Lindquist <jarllindquist@gmail.com>
2016-02-12 08:28:38 +0100

    påbyggnad av ny version av server filen för simple_api

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 
+var get = function(req,res){
 
+}
+var post = function(req,res){
+	
+}
+var delete = function(req,res){
+	
+}
+var put = function(req,res){
+	
+}
 
 
 var server = app.listen(3000, function() {
