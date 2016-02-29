66e0266144314ea1e3b1217fec2abbdcc64dbd9b
Jarl Holmberg Lindquist <jarllindquist@gmail.com>
2016-02-29 20:20:43 +0100

    all upload

--- server.js
+++ server.js
@@ -7,19 +7,47 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
+data = [];
+
 
 var get = function(req,res){
-	res.json
+	res.json({
+		status: "OK",
+		resultify: "GET Multiple"
+	})
 }
 
 var getOne = function(req,res){
+var base = parseFloat(req.params.base);
+	var exponent = parseFloat(req.params.second);
+	var resultat = Math.pow(base, exponent);
+
+	if( isNaN(resultat) || resultat === null || resultat === undefined){
+		res.json({status: "Err"});
+	}
+	else{
+	res.json({
+		status: "OK",
+		result: resultat
+	})
+	}
+
 
 }
 
 var post = function(req,res){
-	
+	data.push({
+
+		id:data.lenght+1,
+		name: req.body.name,
+		postning: req.body.postning	
+		})
 }
 
+
+var postWithGet = function(req,res){
+
+}
 var remove = function(req,res){
 	
 }
@@ -28,8 +56,15 @@ var put = function(req,res){
 	
 }
 
+var start = function() {
+	var port = server.address().port;
+	
 
-app.get('/update', get);
+	console.log("Server started. Listening to connections on port " + port );
+})
+
+app.get('/pow/:base/:second', getOne);
+app.get('/api/data', get);
 // app.get('/', function (req,res){
 // 	res.sendFile('index.html');
 
@@ -37,9 +72,4 @@ app.get('/update', get);
 
 app.use(express.static('public'));
 
-var server = app.listen(3001, function() {
-	var port = server.address().port;
-	
-
-	console.log("Server started. Listening to connections on port " + port );
-})
+var server = app.listen(80, start); 
